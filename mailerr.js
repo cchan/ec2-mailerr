7891c81c1c77874f337a17a3a44c4d63528660a1
Clive Chan <cc@clive.io>
2016-08-11 06:23:15 -0400

    fix the major code injection hole looool

--- mailerr.js
+++ mailerr.js
@@ -4,35 +4,38 @@ var exec = require('child_process').exec;
 var PORT = 55923;
 var helmet = require('helmet');
 var multipart = require('connect-multiparty');
-var http = require('http');
+var https = require('https');
+var qs = require('querystring');
 
 app.use(helmet());
 app.use(multipart());
 
 app.post(secrets.path, function (req, res) {
-  var event = req.body['event'].replace(/\W/g, '');
-  var recip = req.body['recipient'].replace(/[^0-9a-zA-Z@\./g, '');
-  var client = http.createClient(80, secrets.mailapi);
-  var request = client.request('POST', secrets.apiroute, {
-    'Host': secrets.mailapi,
-    'Authorization': 'Basic ' + new Buffer(secrets.apiuser + ':' + secrets.apikey).toString('base64')
-  });
-
-  + " -F from='"            + secrets.mailbot + "'"
-  + " -F to='"              + secrets.recipients + "'"
-  + " -F subject='Mailerr " + event + ": " + recip + "'"
-  + " -F text='"            + event + ": " + recip + "\r\n" + secrets.response + "'",
-  function(error, stdout, stderr){
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
+  var r = https.request({
+    hostname: 'api.mailgun.net',
+    path: '/v3/clive.io/messages',
+    method: 'POST',
+    headers:{
+      'Authorization': 'Basic ' + new Buffer('api:' + secrets.apikey).toString('base64')
     }
-    else{
-      res.send('stdout: '+ stdout + '\r\nstderr: ' + stderr);
-      res.end();
-    }
+  }, function(){
+    console.log('sent');
+    res.send('sent');
   });
+  
+  r.write(qs.stringify({
+    from: 'mailbot@clive.io',
+    to: 'cc@clive.io',
+    subject: 'Mailerr ' + req.body['event'] + ": " + req.body['recipient'],
+    text: 'https://mailgun.com/app/logs/clive.io?event=failed-permanent&event=failed-temporary&event=complained&event=rejected\r\n' + JSON.stringify(req.body)
+  });
+  
+  r.on('error', function(e){
+    console.error(e.message);
+    res.end();
+  });
+  
+  r.end();
 });
 
 app.listen(PORT, 'localhost', function(){
